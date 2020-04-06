1d598bb5a03ae1defc74be87097e7e725477e17c
jdjwelsch <mail@jonawelsch.de>
2020-04-06 22:52:32 +0200

    first basic working colorpicker

--- raspi/frontend/src/components/SingleLedControl.vue
+++ raspi/frontend/src/components/SingleLedControl.vue
@@ -1,86 +1,112 @@
 <template>
-  <div id="SingleLedControl">
-    <button @click='toggle()'>On/Off</button>
-    <span v-show='status'>LED on</span>
-    <span v-show='!status'>LED off</span>
+    <div id="SingleLedControl">
+        <button @click='toggle()'>On/Off</button>
+        <span v-show='power'>LED on</span>
+        <span v-show='!power'>LED off</span>
 
-    <range-slider
-      class="slider"
-      min="0"
-      max="100"
-      step="1"
-      v-model="brightnessSliderVal">
-    </range-slider>
+        <range-slider
+                class="slider"
+                min="0"
+                max="100"
+                step="1"
+                v-model="brightnessSliderVal">
+        </range-slider>
 
-    <div id="hsv">
-      <h2>Here should be the HSV slider</h2>
-      <slider-picker v-model="colors" @input="set_color"></slider-picker>
+        <div id="hsv">
+            <h2>Here should be the HSV slider</h2>
+            <slider-picker v-model="hslColor"></slider-picker>
+        </div>
+
     </div>
-
-  </div>
 </template>
 
 <script>
-import axios from 'axios';
-import RangeSlider from 'vue-range-slider';
-import 'vue-range-slider/dist/vue-range-slider.css';
-import slider from 'vue-color'
+    import axios from 'axios';
+    import RangeSlider from 'vue-range-slider';
+    import 'vue-range-slider/dist/vue-range-slider.css';
+    import {Slider} from 'vue-color';
 
 
-export default {
-  name: 'SingleLedControl',
-  components: {
-    RangeSlider,
-    'slider-picker': slider,
-  },
-  props: ['name'],
+    export default {
+        name: 'SingleLedControl',
+        components: {
+            RangeSlider,
+            'slider-picker': Slider,
+        },
+        props: ['name'],
 
-  data: function() {
-    return {
-        status: false,
-        brightnessSliderVal: 50,
-        colors: {h: 233, s: 100, v: 100}
+        data: function () {
+            return {
+                power: false,
+                brightnessSliderVal: 50,
+                hslColor: {r: 127, g: 100, b: 0.5}
+            }
+        },
+        methods: {
+            toggle: function () {
+                this.power = !this.power;
+                this.switch_led(this.power);
+            },
+
+            switch_led: function (state) {
+                const path = 'http://192.168.0.78:5000/devices/' + this.name;
+                const rgb_off = {'r': 0, 'g': 0, 'b': 0};
+
+                if (state) {
+                    axios.put(path, {'r': this.hslColor.rgba.r, 'g': this.hslColor.rgba.g, 'b': this.hslColor.rgba.b});
+                    console.log({'r': this.hslColor.rgba.r, 'g': this.hslColor.rgba.g, 'b': this.hslColor.rgba.b});
+                }
+
+                if (!state) {
+                    axios.put(path, rgb_off);
+                }
+            },
+
+            set_led_brightness: function (val) {
+                this.hslColor.hsl.l = val / 100;
+                console.log(this.hslColor)
+                const path = 'http://192.168.0.78:5000/devices/' + this.name
+                const rgb = {'r': this.hslColor.rgba.r, 'g': this.hslColor.rgba.g, 'b': this.hslColor.rgba.b};
+                axios.put(path, rgb);
+
+            },
+
+            set_color() {
+                // TODO send RGB color to backend
+                const path = 'http://192.168.0.78:5000/devices/' + this.name
+                const rgb = {'r': this.hslColor.rgba.r, 'g': this.hslColor.rgba.g, 'b': this.hslColor.rgba.b};
+                axios.put(path, rgb);
+
+            },
+        },
+
+        watch: {
+            brightnessSliderVal() {
+                this.set_led_brightness();
+            },
+
+
+            hslColor(hslColor) {
+                //hsv2rgb()
+                console.log(hslColor.rgba.r);
+                console.log(hslColor.rgba.g);
+                console.log(hslColor.rgba.b);
+                this.set_color();
+
+                if (hslColor.hsl.l > 0){
+                    this.power = true;
+                }
+
+            }
+
+        },
+
+        created() {
+            console.log(Slider)
+            console.log(this.hslColor)
+        }
+
     }
-  },
-  methods: {
-    toggle: function() {
-      this.status = !this.status;
-      this.switch_led(this.status);
-    },
-    switch_led: function(state) {
-      const path = 'http://192.168.0.78:5000/devices/' + this.name;
-      const data_on = {'r': 255, 'g': 0, 'b': 0};
-      const data_off = {'r': 0, 'g': 0, 'b': 0};
-
-      if (state) {
-        axios.put(path, data_on);
-        console.log(data_on);
-      }
-
-      if (!state) {
-        axios.put(path, data_off);
-      }
-    },
-    set_led_brightness: function(val) {
-      const data_bright = {'r': 2.55 * val, 'g': 0, 'b': 0};
-      const path = 'http://192.168.0.78:5000/devices/' + this.name
-      // send put request to backend
-      axios.put(path, data_bright);
-    },
-
-    set_color (colors) {
-      this.colors = colors;
-      // TODO send RGB color to backend
-    }
-
-  },
-  watch: {
-    brightnessSliderVal: function(val) {
-      this.set_led_brightness(val);
-    }
-  }
-
-}
 </script>
 
 <style scoped>
